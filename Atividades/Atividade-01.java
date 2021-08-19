14edf6ab18442aed26162d402b7938d37072ef69
Rafael Neves <43051996+rafaelfneves@users.noreply.github.com>
2021-08-19 12:16:17 -0300

    Add files via upload

---
+++ Atividades/Atividade-01.java
@@ -0,0 +1,12 @@
+/** Rafael Fernandes das Neves 50015993 */
+
+public class Tabuada {
+      
+    public static void main(String[] args) {
+        for (int i=1; i <= 10; i++){
+            System.out.printf("10 x %d = %d\n",i,i*10);
+        }
+
+    }
+
+}
